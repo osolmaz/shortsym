c7cae012c7148446ec4233094976a2f7ffb28a51
onursolmaz <onursolmaz@gmail.com>
2017-01-14 18:13:04 +0300

    Minor

---
+++ merge_package.sh
@@ -0,0 +1,22 @@
+cat shortsym_header.sty > shortsym.sty
+
+cat shortsym_bold.sty >> shortsym.sty
+cat shortsym_italic.sty >> shortsym.sty
+cat shortsym_upright.sty >> shortsym.sty
+cat shortsym_upright_bold.sty >> shortsym.sty
+cat shortsym_amsbb.sty >> shortsym.sty
+cat shortsym_sans.sty >> shortsym.sty
+cat shortsym_sans_bold.sty >> shortsym.sty
+cat shortsym_cal.sty >> shortsym.sty
+cat shortsym_cal_bold.sty >> shortsym.sty
+cat shortsym_bbm.sty >> shortsym.sty
+cat shortsym_frak.sty >> shortsym.sty
+# cat shortsym_bbold.sty >> shortsym.sty
+# cat shortsym_euscript.sty >> shortsym.sty
+
+# Greek letters
+
+cat shortsym_greek_bold.sty >> shortsym.sty
+# cat shortsym_greek_slanted_uppercase.sty >> shortsym.sty
+# cat shortsym_greek_upright_lowercase.sty >> shortsym.sty
+# cat shortsym_greek_upright_bold_lowercase.sty >> shortsym.sty
